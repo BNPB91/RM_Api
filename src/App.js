be3cd9f839cbeafe1ccfad409c4034902f0625ef
Bastian Pavez <bastian.npb@gmail.com>
2021-09-23 23:40:50 -0300

    base

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import React, {useState, useEffect} from 'react';
+
+
+// Styles 
+import "./App.css";
+
+
+// COMPONENTS
+import SingleCharacter from './components/SingleCharacter.jsx';
+
+
+const App = () => {
+
+
+  // Estado del componente
+  const [characters, setCharacters] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const handleFetchAPI = async () => {
+
+    try {
+
+    const response = await fetch(`https://rickandmortyapi.com/api/character/?page=${page}`);
+
+    const { results, info} = await response.json();
+
+      // Guardar Results en estado 
+    console.log(results);
+    console.log(info);
+
+    // setCharacters(results);
+    setCharacters([...characters,...results]);
+      
+    } catch (error) {
+      console.log(error);
+    }
+
+};
+
+
+  const handleNewPage = () => {
+      setPage(page + 1);
+  };
+
+
+
+  useEffect (() => { 
+    handleFetchAPI();
+    }, [page]);
+
+
+  return (
+    <div className="App">
+    <section className="characters-container">
+    
+      {characters.map(character => (
+        <SingleCharacter 
+        name={character.name} 
+        avatar={character.image} 
+        key={character.id}
+        />
+      ))}
+
+      <button className="more-btn" onClick={handleNewPage}>
+      Ver más
+      </button>
+
+      </section>
+
+       
+
+
+    </div>
+
+  );
+};
+
+export default App;
---
+++ src/components/SingleCharacter.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// Style
+import "../styles/Character.css";
+
+
+const SingleCharacter = props => {
+    
+    const {name, avatar} = props;
+    return (
+        <div className="single-card">
+            <img src={avatar} alt={name}/>
+            <h1>{name}</h1>
+        </div>
+        );
+
+};
+
+export default SingleCharacter;
